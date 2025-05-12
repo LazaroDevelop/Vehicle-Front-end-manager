fe61d4b8cc1cf2e08ae601c29e0a9781c48713fd
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-12 13:26:47 -0400

    fill list component

--- src/app/domains/vehicles/pages/list/list.component.ts
+++ src/app/domains/vehicles/pages/list/list.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
+import { Vehicle } from '../../../shared/models/vehicle.model';
+import { CommonModule } from '@angular/common';
+import { VehicleComponent } from '../../components/vehicle/vehicle.component';
+import { VehicleService } from '../../../shared/services/vehicle.service';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
-  selector: 'app-list',
-  imports: [],
+  selector: 'list-ui',
+  imports: [CommonModule, VehicleComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
+  standalone: true,
 })
 export class ListComponent {
+  @Input() category!: string;
+  vehicles = signal<Vehicle[]>([]);
+  private vehicleSerivce = inject(VehicleService);
+  categories: string[] = ['ELECTRICAL', 'GASOLINE', 'DIESEL'];
 
+  ngOnInit() {
+    this.getVehicles();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const category = changes['category'];
+    if (category) {
+      this.getVehicles();
+    }
+  }
+
+  getVehicles() {
+    this.vehicleSerivce.getVehicles(this.category).subscribe({
+      next: (data) => {
+        const allVehicles = [
+          ...(data._embedded.electricalModelList || []),
+          ...(data._embedded.gasolineModelList || []),
+          ...(data._embedded.dieselModelList || []),
+        ];
+        this.vehicles.set(allVehicles);
+      },
+    });
+  }
 }
