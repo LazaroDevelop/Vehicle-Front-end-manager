ee08b296a43ebc3391771abaab021bc7bf39b73c
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-13 08:25:30 -0400

    add not-found component

---
+++ src/app/domains/info/pages/not-found/not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [RouterLinkWithHref],
+  standalone: true,
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css',
+})
+export class NotFoundComponent {}
